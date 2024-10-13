4d9f0a88a49462713e9b80faab48c629182ed66d
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-10-13 14:55:20 +0200

    setup initial window

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import QTimer, QTime, Qt
+
+class DigitalClock(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        pass
+    
+if __name__ == "__main__":
+      app = QApplication(sys.argv)
+      clock = DigitalClock()
+      clock.show()
+      sys.exit(app.exec_())
+        
