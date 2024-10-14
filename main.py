1a358d853a6246f086efb798ac4ea1aa1cf422f9
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-10-14 13:40:46 +0200

    configure to update time every second

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PyQt5.QtCore import QTimer, QTime, Qt
 class DigitalClock(QWidget):
     def __init__(self):
         super().__init__()
-        self.time_label = QLabel("12:00:00", self)
+        self.time_label = QLabel(self)
         self.timer = QTimer(self)
         self.initUI()
         
@@ -25,6 +25,15 @@ class DigitalClock(QWidget):
                                       )
         
         self.setStyleSheet("background: black;")
+        
+        self.timer.timeout.connect(self.update_time)
+        self.timer.start(1000)
+        
+        self.update_time()
+        
+    def update_time(self):
+        current_time = QTime.currentTime().toString("hh:mm:ss AP")
+        self.time_label.setText(current_time)
     
 if __name__ == "__main__":
       app = QApplication(sys.argv)
